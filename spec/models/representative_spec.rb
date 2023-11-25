c2f93102d06702e2112fefb0e6f963a1a3fe6c08
Tiffany Lam <tiffanylam14@berkeley.edu>
2023-11-25 06:08:28 +0000

    [test]syntax stuff

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -1,30 +1,29 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 RSpec.describe Representative, type: :model do
-  describe '.civic_api_to_representative_params' do 
-    it 'creates representatives without errors for duplicate input' do 
-      rep_info = double('rep_info', 
-        officials: [
-          double('official', name: 'Bill Smith', address: [double('address', locationName: '123 Flame Ave', line1: 'Apt 1', line2: '', line3: '', city: 'Perris', state: 'CA', zip: '12345')], party: 'Party', photoUrl: "url"),
-          double('official', name: 'Hannah Smith', address: [double('address', locationName: '123 Flame Ave', line1: 'Apt 1', line2: '', line3: '', city: 'Perris', state: 'CA', zip: '12345')], party: 'Party', photoUrl: "url"),
+  describe '.civic_api_to_representative_params' do
+    it 'creates representatives without errors for duplicate input' do
+      rep_info = double('rep_info',
+                        officials: [
+                          double('official', name: 'Bill Smith',
+address: [double('address', locationName: '123 Flame Ave', line1: 'Apt 1', line2: '', line3: '', city: 'Perris', state: 'CA', zip: '12345')], party: 'Party', photoUrl: 'url'),
+                          double('official', name: 'Hannah Smith',
+address: [double('address', locationName: '123 Flame Ave', line1: 'Apt 1', line2: '', line3: '', city: 'Perris', state: 'CA', zip: '12345')], party: 'Party', photoUrl: 'url')
 
-        ],
-        offices: [
-          double('office', name: 'Office1', division_id: '1', official_indices: [0, 1]),
-        ]
-      )
+                        ],
+                        offices:   [
+                          double('office', name: 'Office1', division_id: '1', official_indices: [0, 1])
+                        ])
 
-      #create representative with same name to check if it errors or not (it shouldn't)
-      Representative.create!(name: 'Bill Smith', ocdid: '1', title: 'Office1')
+      # create representative with same name to check if it errors or not (it shouldn't)
+      described_class.create!(name: 'Bill Smith', ocdid: '1', title: 'Office1')
 
-      expect {
-        Representative.civic_api_to_representative_params(rep_info)
-      }.not_to raise_error
+      expect do
+        described_class.civic_api_to_representative_params(rep_info)
+      end.not_to raise_error
 
-      expect(Representative.count).to eq(2)
-
-  
+      expect(described_class.count).to eq(2)
     end
   end
-end 
-
-      
+end
