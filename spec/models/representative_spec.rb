8811850c676ab010fe7a6a4707c1b704e9d7b433
Tiffany Lam <tiffanylam14@berkeley.edu>
2023-11-27 07:59:53 +0000

    robocop changes

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -4,17 +4,20 @@ require 'rails_helper'
 RSpec.describe Representative, type: :model do
   describe '.civic_api_to_representative_params' do
     it 'creates representatives without errors for duplicate input' do
-      rep_info = instance_double('rep_info',
-                        officials: [
-                          instance_double('official', name: 'Bill Smith',
-address: [instance_double('address', locationName: '123 Flame Ave', line1: 'Apt 1', line2: '', line3: '', city: 'Perris', state: 'CA', zip: '12345')], party: 'Party', photoUrl: 'url'),
-                          instance_double('official', name: 'Hannah Smith',
-address: [instance_double('address', locationName: '123 Flame Ave', line1: 'Apt 1', line2: '', line3: '', city: 'Perris', state: 'CA', zip: '12345')], party: 'Party', photoUrl: 'url')
-
-                        ],
-                        offices:   [
-                          instance_double('office', name: 'Office1', division_id: '1', official_indices: [0, 1])
-                        ])
+      rep_info = instance_double(rep_info,
+                                 officials: [
+                                   instance_double(official, name: 'Bill Smith',
+         address: [instance_double(address, locationName: '123 Flame Ave', line1: 'Apt 1',
+                                    line2: '', line3: '', city: 'Perris', state: 'CA', zip: '12345')],
+                                    party: 'Party', photoUrl: 'url'),
+                                   instance_double(official, name: 'Hannah Smith',
+         address: [instance_double(address, locationName: '123 Flame Ave',
+         line1: 'Apt 1', line2: '', line3: '', city: 'Perris',
+         state: 'CA', zip: '12345')], party: 'Party', photoUrl: 'url')
+                                 ],
+                                 offices:   [
+                                   instance_double(office, name: 'Office1', division_id: '1', official_indices: [0, 1])
+                                 ])
 
       # create representative with same name to check if it errors or not (it shouldn't)
       described_class.create!(name: 'Bill Smith', ocdid: '1', title: 'Office1')
