aa03b28bb5887ea35302071a7c00fc07285bdc80
Bowen Fan <bowen.fan@berkeley.edu>
2023-11-28 07:05:36 +0000

    fixed rubocop errors from another branch

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -4,6 +4,10 @@ require 'rails_helper'
 
 describe Representative, type: :model do
   describe '.civic_api_to_representative_params' do
+    before do
+      described_class.create!(name: 'Bill Smith', ocdid: '1', title: 'Office1')
+    end
+
     let(:rep_info) do
       instance_double(RepInfo,
                       officials: [
@@ -15,26 +19,24 @@ describe Representative, type: :model do
                       ])
     end
 
-    before do
-      described_class.create!(name: 'Bill Smith', ocdid: '1', title: 'Office1')
-    end
-
     it 'creates representatives without errors for duplicate input' do
       expect do
         described_class.civic_api_to_representative_params(rep_info)
       end.not_to raise_error
     end
 
-    it 'finds Bill Smith and Jacob Willows in the database' do
+    it 'finds Bill Smith in the database' do
       described_class.civic_api_to_representative_params(rep_info)
-
       expect(described_class.find_by(name: 'Bill Smith')).not_to be_nil
+    end
+
+    it 'finds Jacob Willows in the database' do
+      described_class.civic_api_to_representative_params(rep_info)
       expect(described_class.find_by(name: 'Jacob Willows')).not_to be_nil
     end
 
     it 'has a count of 2 in the database' do
       described_class.civic_api_to_representative_params(rep_info)
-
       expect(described_class.count).to eq(2)
     end
   end
