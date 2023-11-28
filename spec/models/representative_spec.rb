a35cf413aeae38d98b3ad92516372c7e35c4d302
Bowen Fan <bowen.fan@berkeley.edu>
2023-11-28 06:35:16 +0000

    Fixed some rubocop errors

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -1,32 +1,41 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe Representative, type: :model do
-  describe '.civic_api_to_representative_params' do 
-    it 'creates representatives without errors for duplicate input' do 
-      rep_info = double('rep_info', 
-        officials: [
-          double('official', name: 'Bill Smith'),
-          double('official', name: 'Jacob Willows')
-        ],
-        offices: [
-          double('office', name: 'Office1', division_id: '1', official_indices: [0, 1]),
-        ]
-      )
+  describe '.civic_api_to_representative_params' do
+    let(:rep_info) do
+      instance_double(RepInfo,
+                      officials: [
+                        instance_double(Official, name: 'Bill Smith'),
+                        instance_double(Official, name: 'Jacob Willows')
+                      ],
+                      offices:   [
+                        instance_double(Office, name: 'Office1', division_id: '1', official_indices: [0, 1])
+                      ])
+    end
 
-      #create representative with same name to check if it errors or not (it shouldn't)
-      Representative.create!(name: 'Bill Smith', ocdid: '1', title: 'Office1')
+    before do
+      described_class.create!(name: 'Bill Smith', ocdid: '1', title: 'Office1')
+    end
 
-      expect {
-        Representative.civic_api_to_representative_params(rep_info)
-      }.not_to raise_error
+    it 'creates representatives without errors for duplicate input' do
+      expect do
+        described_class.civic_api_to_representative_params(rep_info)
+      end.not_to raise_error
+    end
 
-      expect(Representative.find_by(name: 'Bill Smith')).not_to be_nil
+    it 'finds Bill Smith and Jacob Willows in the database' do
+      described_class.civic_api_to_representative_params(rep_info)
 
-      expect(Representative.find_by(name: 'Jacob Willows')).not_to be_nil
+      expect(described_class.find_by(name: 'Bill Smith')).not_to be_nil
+      expect(described_class.find_by(name: 'Jacob Willows')).not_to be_nil
+    end
 
-      expect(Representative.count).to eq(2)
+    it 'has a count of 2 in the database' do
+      described_class.civic_api_to_representative_params(rep_info)
+
+      expect(described_class.count).to eq(2)
     end
   end
-end 
-
-      
+end
